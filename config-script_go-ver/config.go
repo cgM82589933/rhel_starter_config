e531c7290d91567995edd85f7fb381bb5059ba7e
Chris Girvin <cgm82589933@outlook.com>
2022-06-28 01:27:38 -0400

    initial commit

---
+++ config-script_go-ver/config.go
@@ -0,0 +1,15 @@
+// @Title
+// @Description
+// @Author
+// @Update
+
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func main() {
+	log.Fatal(fmt.Println("Let's configure this server!"))
+}
---
+++ config-script_go-ver/go.mod
@@ -0,0 +1,3 @@
+module github.com/cgm82589933/rhel_starter_config/config-script_go-ver
+
+go 1.18
